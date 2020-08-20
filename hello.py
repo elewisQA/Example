a2c944e1501f26579bfecd501131d7d5a5d80e51
Elizabeth Lewis <elewis@academytrainee.com>
2020-08-20 11:30:03 +0100

    Added new python program.

--- hello.py
+++
@@ -1,3 +0,0 @@
-# This is a sample python program
-print("Hello!")
-print("World!")
---
+++ python/sums.py
@@ -0,0 +1,3 @@
+a = int(input("Type in a number: "))
+b = int(input("Type in another number: "))
+print("a + b = {0}".format(a + b))
